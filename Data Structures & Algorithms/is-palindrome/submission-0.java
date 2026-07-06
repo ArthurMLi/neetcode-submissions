3756ac8a5504e53446366857bad41a8f3ddc809b
ArthurMLi <167795406+ArthurMLi@users.noreply.github.com>
2026-07-05 22:19:34 -0300

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int len = s.length();
+        int j = len - 1; 
+        for(int i = 0; i < j;){
+            if(isValidChar(s.charAt(i))){
+                if(isValidChar(s.charAt(j))){
+                    if(Character.toUpperCase(s.charAt(i)) == Character.toUpperCase(s.charAt(j))){
+                        i++;
+                        j--;
+                    }
+                    else{ return false;}
+                }else{
+                    j--;
+                }
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return true;
+    }
+
+    private boolean isValidChar(char c){
+        if(c >= 'A' && c <= 'Z')return true;
+        if(c >= 'a' && c <= 'z')return true;
+        if(c >= '0' && c <= '9')return true;
+        return false;
+    }
+}
